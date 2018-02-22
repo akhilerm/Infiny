d8e800afeef2371fced4e924fc8eabb5815472d0
Koso <danilo.flemma2012@hotmail.it>
2018-02-22 16:12:16 +0100

    Bundled jquery

--- infiny.js
+++ infiny.js
@@ -1,3 +1,7 @@
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+
 contacts = [];
 chat_div = [];
 
